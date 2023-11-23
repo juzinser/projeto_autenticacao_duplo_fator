36507e96b3b50c43276214d642dccf341a4fef3b
Sobraloser <Gabriel.Sobral1367@gmail.com>
2023-11-23 02:21:22 +0000

    fix: changing port to deploy

--- api/server/index.js
+++ api/server/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT ?? 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
